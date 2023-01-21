ad971e6c7911a3563c2623570b27b6c7f341e0b6
AceVault <96090803+AceVault@users.noreply.github.com>
2023-01-21 09:21:42 -0500

    Update AutoDSR.ps1

--- AutoDSR.ps1
+++ AutoDSR.ps1
@@ -1,3 +1,5 @@
+taskkill /f /im RobloxPlayerBeta.exe
+Start-Sleep 1
 Invoke-WebRequest https://github.com/AceVault/AutoDSR/raw/main/ouch.ogg -OutFile $env:USERPROFILE/ouch.ogg
 $location = Get-ChildItem "$env:LOCALAPPDATA\Roblox\Versions\" -Directory
 foreach ($folder in $location) {Copy-Item -Path "$env:USERPROFILE/ouch.ogg" -Destination "$env:LOCALAPPDATA\Roblox\Versions\$folder\content\sounds" -Recurse}
