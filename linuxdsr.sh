5fda2b1bac32d40520c965fed10a0679e7d6aee7
AceVault <96090803+AceVault@users.noreply.github.com>
2023-02-11 08:28:22 -0500

    Update linuxdsr.sh

--- linuxdsr.sh
+++ linuxdsr.sh
@@ -3,5 +3,6 @@ curl -L -O https://github.com/AceVault/AutoDSR/raw/main/ouch.ogg
 
 #copies the death sound to every wine prefix and every download folder 
 #this will fail if you dont have parallel installed 
+killall RobloxPlayerBet 
 parallel cp $HOME/ouch.ogg ::: $HOME/.local/share/grapejuice/prefixes/*/drive_c/"Program Files (x86)"/Roblox/Versions/version*/content/sounds/
 echo "death sound replaced"
